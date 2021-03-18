c61a4627f1a0a8ffb4b1dc590b695f4fb011dd63
xdd44-utopia <xdd44.utopia@gmail.com>
2021-03-18 10:00:08 +0800

    initiate

---
+++ Client/Assets/Scripts/ClientController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+
+public class ClientController : MonoBehaviour {
+
+	public GameObject obj;
+	public Camera renderCamera;
+
+	private Color disconnectColor = new Color(0.8156f, 0.3529f, 0.4313f);
+	//private Color connectColor = new Color(0.5254f, 0.7568f, 0.4f);
+	private Color connectColor = new Color(0f, 0f, 0f);
+
+	private Vector3 pos = new Vector3(0, 0, 0);
+	private Vector3 view = new Vector3(0, 0, 0);
+	private bool isRefreshed = false;
+
+	private TcpClient socketConnection;
+	private Thread clientReceiveThread;
+	
+	void Start () {
+		socketConnection = null;
+		//ConnectToTcpServer("1.1.1.1");
+	}
+	
+	void Update () {
+		renderCamera.backgroundColor = (socketConnection == null ? disconnectColor : connectColor);
+		if (isRefreshed) {
+			obj.GetComponent<ModelController>().receivePos(pos);
+			obj.GetComponent<ModelController>().receiveView(view);
+			isRefreshed = false;
+		}
+	}
+	
+	public void ConnectToTcpServer (string ipText) {
+		try {
+			clientReceiveThread = new Thread(() => ListenForData(ipText));
+			clientReceiveThread.IsBackground = true;
+			clientReceiveThread.Start();
+		}
+		catch (Exception e) {
+			Debug.Log("On client connect exception " + e);
+		}
+	}
+	
+	private void ListenForData(string ipText) {
+		socketConnection = null;
+		try {
+			socketConnection = new TcpClient(ipText, 8052);
+			Byte[] bytes = new Byte[1024];
+			while (true) {
+				using (NetworkStream stream = socketConnection.GetStream()) {
+					int length;
+					while ((length = stream.Read(bytes, 0, bytes.Length)) != 0) {
+						var incommingData = new byte[length];
+						Array.Copy(bytes, 0, incommingData, 0, length);
+						string serverMessage = Encoding.ASCII.GetString(incommingData);
+						getVector(serverMessage);
+						isRefreshed = true;
+					}
+				}
+			}
+		}
+		catch (SocketException socketException) {
+			Debug.Log("Socket exception: " + socketException);
+		}
+	}
+	
+	public void sendMessage(Vector3 pos) {
+		if (socketConnection == null) {
+			return;
+		}
+		try {		
+			NetworkStream stream = socketConnection.GetStream();
+			if (stream.CanWrite) {
+				string clientMessage = pos.x + "," + pos.y + "," + pos.z + ",";
+				byte[] clientMessageAsByteArray = Encoding.ASCII.GetBytes(clientMessage);
+				stream.Write(clientMessageAsByteArray, 0, clientMessageAsByteArray.Length);
+				Debug.Log("Client sent his message - should be received by server");
+			}
+		}
+		catch (SocketException socketException) {
+			Debug.Log("Socket exception: " + socketException);
+		}
+	}
+
+	private void getVector(string str) {
+		string[] temp = str.Split(',');
+		pos = new Vector3(System.Convert.ToSingle(temp[0]), System.Convert.ToSingle(temp[1]), System.Convert.ToSingle(temp[2]));
+		view = new Vector3(System.Convert.ToSingle(temp[3]), System.Convert.ToSingle(temp[4]), System.Convert.ToSingle(temp[5]));
+	}
+}
---
+++ Server/Assets/Scripts/ServerController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ServerController : MonoBehaviour {
+
+	public Text ipText;
+	public Text rcvText;
+	public GameObject obj;
+	public Camera renderCamera;
+
+	private Color disconnectColor = new Color(0.8156f, 0.3529f, 0.4313f);
+	//private Color connectColor = new Color(0.5254f, 0.7568f, 0.4f);
+	private Color connectColor = new Color(0f, 0f, 0f);
+
+	private Vector3 pos = new Vector3(0, 0, 0);
+	private bool isRefreshed = false;
+
+	private TcpListener tcpListener;
+	private Thread tcpListenerThread;
+	private TcpClient connectedTcpClient;
+	private string rcvMsg = "";
+
+	private bool noConnection = true;
+	
+	void Start () {
+		tcpListenerThread = new Thread (new ThreadStart(ListenForIncommingRequests));
+		tcpListenerThread.IsBackground = true;
+		tcpListenerThread.Start();
+	}
+	
+	void Update () {
+		ipText.text = getIPAddress();
+		rcvText.text = rcvMsg;
+		renderCamera.backgroundColor = (connectedTcpClient == null ? disconnectColor : connectColor);
+		if (connectedTcpClient != null && noConnection) {
+			sendMessage();
+			noConnection = false;
+		}
+		if (isRefreshed) {
+			obj.GetComponent<ModelController>().receivePos(pos);
+			isRefreshed = false;
+		}
+	}
+	
+	private void ListenForIncommingRequests () {
+		try {
+			tcpListener = new TcpListener(IPAddress.Any, 8052);
+			tcpListener.Start();
+			Debug.Log("Server is listening");
+			Byte[] bytes = new Byte[1024];
+			while (true) {
+				using (connectedTcpClient = tcpListener.AcceptTcpClient()) {
+					using (NetworkStream stream = connectedTcpClient.GetStream()) {
+						int length;
+						while ((length = stream.Read(bytes, 0, bytes.Length)) != 0) {
+							var incommingData = new byte[length];
+							Array.Copy(bytes, 0, incommingData, 0, length);
+							string clientMessage = Encoding.ASCII.GetString(incommingData);
+							rcvMsg = clientMessage;
+							pos = getVector(clientMessage);
+							isRefreshed = true;
+						}
+					}
+				}
+			}
+		}
+		catch (SocketException socketException) {
+			Debug.Log("SocketException " + socketException.ToString());
+		}
+	}
+	
+	public void sendMessage() {
+		if (connectedTcpClient == null) {
+			return;
+		}
+		
+		try {			
+			NetworkStream stream = connectedTcpClient.GetStream();
+			if (stream.CanWrite) {
+				string serverMessage = obj.transform.position.x + "," + obj.transform.position.y + "," + obj.transform.position.z + "," + renderCamera.transform.position.x + "," + renderCamera.transform.position.y + "," + renderCamera.transform.position.z + ",";
+				byte[] serverMessageAsByteArray = Encoding.ASCII.GetBytes(serverMessage);
+				stream.Write(serverMessageAsByteArray, 0, serverMessageAsByteArray.Length);
+				Debug.Log("Server sent his message - should be received by client");
+			}
+		}
+		catch (SocketException socketException) {
+			Debug.Log("Socket exception: " + socketException);
+		}
+	}
+	
+	private string getIPAddress() {
+		var host = Dns.GetHostEntry(Dns.GetHostName());
+		foreach (var ip in host.AddressList) {
+			if (ip.AddressFamily == AddressFamily.InterNetwork)
+			{
+				return ip.ToString();
+			}
+		}
+		throw new System.Exception("No network adapters with an IPv4 address in the system!");
+	}
+
+	private Vector3 getVector(string str) {
+		string[] temp = str.Split(',');
+		float x = System.Convert.ToSingle(temp[0]);
+		float y = System.Convert.ToSingle(temp[1]);
+		float z = System.Convert.ToSingle(temp[2]);
+		Vector3 v = new Vector3(x, y, z);
+		return v;
+	}
+
+}
